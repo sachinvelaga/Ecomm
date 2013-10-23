c83e321e25660581111593fa50a721f9ead0ec87
unknown <sachinv@langoor.net>
2013-10-24 02:49:04 +0530

    added related function

--- application/models/product_sku_model.php
+++ application/models/product_sku_model.php
@@ -30,7 +30,7 @@
 		}
 		public function getRelatedProducts($header_id , $sku_code)
 		{
-			$where.="a.header_id = '".$header_id."' and sku_code != '".$sku_code."'";
+			$where ="a.header_id = '".$header_id."' and sku_code != '".$sku_code."'";
 			$this->db->select("a.header_id AS id, b.header_code AS GroupId, a.sku_code AS ProductId, b.name AS MovieTitle, s.name AS Store, c.name AS SubCategory, a.price AS Price, a.shipping_duration AS ShippingDuration");
 			$this->db->join('product_header b','a.header_id = b.id','LEFT');
 			$this->db->join('store s','s.id = b.store_id','LEFT');
